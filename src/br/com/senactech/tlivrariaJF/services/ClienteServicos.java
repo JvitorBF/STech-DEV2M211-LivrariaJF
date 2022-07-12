5b0743c7deb4dfd42907995eb86976cd2eac86ce
JvitoBF <jvitorbf35@gmail.com>
2022-07-12 17:36:22 -0300

    Classe ClienteServicos quase finalizada

--- src/br/com/senactech/tlivrariaJF/services/ClienteServicos.java
+++ src/br/com/senactech/tlivrariaJF/services/ClienteServicos.java
@@ -5,10 +5,31 @@
  */
 package br.com.senactech.TLivrariaJF.services;
 
+import br.com.senactech.TLivrariaJF.DAO.ClienteDAO;
+import br.com.senactech.TLivrariaJF.DAO.DAOFactory;
+import br.com.senactech.tlivrariaJF.model.Cliente;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author João Vitor
  */
 public class ClienteServicos {
+
+    public ArrayList<Cliente> buscarCliente() throws SQLException {
+        ClienteDAO cDAO = DAOFactory.getClienteDAO();
+        return cDAO.selectCliente();
+    }
+
+    public void cadastrarClienteCPF(Cliente cVO) throws SQLException {
+        ClienteDAO cDAO = DAOFactory.getClienteDAO();
+        cDAO.cadastrarClienteCPF(cVO);
+    }
     
+    public void cadastrarClienteCNPJ (Cliente cVO) throws SQLException {
+        ClienteDAO cDAO = DAOFactory.getClienteDAO();
+        cDAO.cadastrarClienteCNPJ(cVO);
+    }
+
 }
